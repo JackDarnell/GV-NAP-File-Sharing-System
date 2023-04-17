9bc5752baef131a000d68fffe55224e630c41afb
Jack Darnell <darnelljack@icloud.com>
2023-04-17 13:48:40 -0400

    added starting files

---
+++ central_server.py
@@ -0,0 +1,2 @@
+#centralized server, indexing all files on the distrubuted system. Other hosts provide info about 
+#files they have to the server, and the server provides info about files to other hosts
---
+++ peer.py
@@ -0,0 +1,1 @@
+#Peer file sharing server, can query server for files and download them
